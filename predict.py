c860b748130f589d8c1db769882c736ff08deca5
Preethi Vaidyanathan <preethi@gaia-scope.com>
2022-01-20 16:16:15 -0500

    Add default settings

--- predict.py
+++ predict.py
@@ -5,6 +5,6 @@ class Predictor(cog.Predictor):
         self.prefix = "hello"
 
     @cog.input("prompts", type=str, help="text prompt")
-    @cog.input("settings", type=str, help="yaml settings")
+    @cog.input("settings", type=str, help="yaml settings", default="default_settings")
     def predict(self, input):
         return io.StringIO("output_file")
